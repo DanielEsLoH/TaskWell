f0dcd7a7d41b558b48c4ab93418de820d9e45e31
Daniel E. Londoño <daniel.esloh@gmail.com>
2025-09-12 09:13:07 -0500

    feat(auth): add initial route setup for authentication pages
    Configured the main authentication routes:
    - auth-layout
    - forgot-password
    - reset-password
    - sign-in
    - sign-up
    - verify-email
    
    Only initial setup, components are not implemented yet.

--- client/app/routes/root/home.tsx
+++ client/app/routes/root/home.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import type { Route } from "../../+types/root";
+import { Link } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,8 +11,17 @@ export function meta({}: Route.MetaArgs) {
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="w-full h-screen flex items-center justify-center gap-4">
+      <Link to="/sign-in">
+        <Button className="bg-blue-500 text-white">Login</Button>
+      </Link>
+      <Link to="/sign-up">
+        <Button variant="outline" className="bg-blue-500 text-white">
+          Sign Up
+        </Button>
+      </Link>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
