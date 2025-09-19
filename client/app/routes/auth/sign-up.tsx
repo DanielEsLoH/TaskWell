dc22f5a6f83e46bc7096685cf4d51da67be87b2d
Daniel E. Londoño <daniel.esloh@gmail.com>
2025-09-18 19:13:59 -0500

    feat: add email verification during user registration
    This commit introduces a new feature to send an email verification link to users upon successful registration.
    
    - The `server/controllers/auth-controller.js` was modified to trigger the email sending process.
    - New files `server/libs/send-email.js` and `server/models/verification.js` were added to handle the email sending logic and token storage, respectively.
    - The `client/app/routes/auth/sign-up.tsx` was updated to handle the response from the server after the user is created.
    - Dependencies in `package.json` and `package-lock.json` were updated to support the new functionality.

--- client/app/routes/auth/sign-up.tsx
+++ client/app/routes/auth/sign-up.tsx
@@ -20,13 +20,14 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { useSignUpMutation } from "@/hooks/use-auth";
 import { toast } from "sonner";
 
 export type SignUpFormData = z.infer<typeof signUpSchema>;
 
 const SignUp = () => {
+  const navigate = useNavigate();
   const form = useForm<SignUpFormData>({
     resolver: zodResolver(signUpSchema),
     defaultValues: {
@@ -42,7 +43,12 @@ const SignUp = () => {
   const handleOnSubmit = (values: SignUpFormData) => {
     mutate(values, {
       onSuccess: () => {
-        toast.success("Account created succesfully");
+        toast.success("Email Verification Required", {
+          description:
+            "Please check your email for a verification link. If you don't see it, please check your spam folder.",
+        });
+        form.reset();
+        navigate("/sign-in");
       },
       onError: (error: any) => {
         const errorMessage =
--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,10 +1,24 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { sendEmail } from "../libs/send-email.js";
+import Verification from "../models/verification.js";
+import aj from "../libs/arcjet.js";
 
 const registerUser = async (req, res) => {
   try {
     const { email, name, password } = req.body;
+
+    const decision = await aj.protect(req, { requested: 1, email }); // Deduct 5 tokens from the bucket
+    console.log("Arcjet decision", decision.isDenied());
+
+    if (decision.isDenied()) {
+      res.writeHead(403, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Invalid email address" }));
+    }
+
     const existingUser = await User.findOne({ email });
+
     if (existingUser) {
       return res.status(400).json({ message: "Email address already in use" });
     }
@@ -17,7 +31,30 @@ const registerUser = async (req, res) => {
       name,
     });
 
-    // TODO: Send verification email to user
+    const verificationToken = jwt.sign(
+      { userId: newUser._id, property: "email-verification" },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    await Verification.create({
+      userId: newUser._id,
+      token: verificationToken,
+      expiresAt: new Date(Date.now() + 1 * 60 * 1000),
+    });
+
+    // send email
+    const verificationLink = `${process.env.FRONTEND_URL}/verify-email?token=${verificationToken}`;
+    const emailBody = `<p>Click <a href="${verificationLink}">here</a> to verify your email</p>`;
+    const emailSubject = "Verify your email";
+
+    const isEmailSent = await sendEmail(email, emailSubject, emailBody);
+
+    if (!isEmailSent) {
+      return res.status(500).json({
+        message: "Failed to send verification email",
+      });
+    }
 
     res.status(201).json({
       message: "Verification email sent. Please check and verify your account.",
